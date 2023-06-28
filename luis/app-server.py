7a3a154cfd29d9ba839acffc904fcf91441adc66
Luis Escolano <escolano@keo-connectivity.de>
2023-06-28 18:01:52 +0200

    Fixed doc from app-cliente and documented/refactored part of app-server

--- luis/app-server.py
+++ luis/app-server.py
@@ -10,7 +10,35 @@ import libserver
 sel = selectors.DefaultSelector()
 
 
+def check_args(args):
+    """
+    Check arguments that we receive asd  input of the program.
+    If not valid, the program ends.
+    """
+    if len(args) != 3:
+        print(f"Usage: {args[0]} <host> <port>")
+        sys.exit(1)
+
+#TODO: check server not responding to request
+def set_up_connection(host, port):
+    """
+    Sets up the server to wait for a client.
+    Host and port args are received via command line args.
+    """
+    host, port = host, int(port)
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Avoid bind() exception: OSError: [Errno 48] Address already in use
+    lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    lsock.bind((host, port))
+    lsock.listen()
+    print(f"Listening on {(host, port)}")
+    lsock.setblocking(False)
+    sel.register(lsock, selectors.EVENT_READ, data=None)
+
 def accept_wrapper(sock):
+    """
+    Defines the behavior when a connection has been established with a client.
+    """
     conn, addr = sock.accept()  # Should be ready to read
     print(f"Accepted connection from {addr}")
     conn.setblocking(False)
@@ -18,20 +46,11 @@ def accept_wrapper(sock):
     sel.register(conn, selectors.EVENT_READ, data=message)
 
 
-if len(sys.argv) != 3:
-    print(f"Usage: {sys.argv[0]} <host> <port>")
-    sys.exit(1)
+# Program start
+check_args(sys.argv)
+set_up_connection(sys.argv[1], sys.argv[2])
 
-host, port = sys.argv[1], int(sys.argv[2])
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Avoid bind() exception: OSError: [Errno 48] Address already in use
-lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-lsock.bind((host, port))
-lsock.listen()
-print(f"Listening on {(host, port)}")
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
-
+#Loop that wait for a client
 try:
     while True:
         events = sel.select(timeout=None)
